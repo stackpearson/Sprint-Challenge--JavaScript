97aa6a77b0bec1c37ce1f72b1d0aa69b3e794a1a
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-05-01 19:13:50 -0600

    object section done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,6 +6,12 @@
   object name, diet, weight, length, period
 */
 
+tyrannosaurus = {diet: 'carnivorous', weight: '7000kg', length: '12m', period: 'Late Cretaceous'};
+
+stegosaurus = {diet: 'herbivorous', weight: '2000kg', length: '9m', period: 'Late Jurassic'};
+
+velociraptor = {diet: 'carnivorous', weight: '15kg', length: '1.8m', period: 'Late Cretaceous'};
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -15,10 +21,10 @@
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
 console.log();
