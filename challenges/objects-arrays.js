c1db610d5c13b4d07b24525ff8405f6940ce4557
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-05-01 19:31:35 -0600

    1st array portion finished

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -27,14 +27,17 @@ console.log(tyrannosaurus.weight);
 console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+tyrannosaurus.roar = function () {
+  return "RAWERSRARARWERSARARARRRR!";
+};
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -58,7 +61,11 @@ const graduates = [
 
 Once you have the new array created, log the result. */
 const universities = [];
-console.log(universities);
+
+graduates.forEach(element => universities.push(element.university));
+
+
+// console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -67,10 +74,17 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
-console.log(contactInfo);
+
+graduates.forEach(element => contactInfo.push(`${element.first_name} ${element.email}`));
+// console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+graduates.forEach(element => {
+  if (element.university.includes('Uni')) {
+    unisWithUni.push(element);
+  }
+})
 console.log(unisWithUni);
 
 
